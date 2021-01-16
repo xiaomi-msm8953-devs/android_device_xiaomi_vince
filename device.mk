6cd4a635687756031208e26a008a086ac74909f1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-16 08:51:21 +0100

    vince: Import audio configs
    * From vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys
    
    Change-Id: I6359d6f2cf5038f0925597228b1b544ff1a37d6d

--- device.mk
+++ device.mk
@@ -12,6 +12,11 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
