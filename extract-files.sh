319c382c36900c8ef18aa525072001473c9d807f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-23 10:42:17 +0100

    vince: Patch goodix libs
    * Remove useless shared libs dlopening
    
    Change-Id: I1b8028c926774d52899f4eaec4d499a104963719

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/hw/gf_fingerprint.default.so \
+        |vendor/lib64/libgoodixfingerprintd_binder.so \
+        |vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so \
+        |vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
+            "${PATCHELF}" --remove-needed "libbacktrace.so" "${2}"
+            "${PATCHELF}" --remove-needed "libkeystore_binder.so" "${2}"
+            "${PATCHELF}" --remove-needed "libkeymaster_messages.so" "${2}"
+            "${PATCHELF}" --remove-needed "libsoftkeymaster.so" "${2}"
+            "${PATCHELF}" --remove-needed "libsoftkeymasterdevice.so" "${2}"
+            "${PATCHELF}" --remove-needed "libunwind.so" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
