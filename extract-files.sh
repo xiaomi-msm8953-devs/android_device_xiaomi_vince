e96bd6e0d527755a1ab3a2f7a744f0e2150f1c6c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-25 23:47:20 +0100

    vince: Ship prebuilt camera provider
    * E CamPrvdr@2.4-legacy: checkCameraVersion: Device 0 has HAL version 5a, which is not supported
    
    Change-Id: I9082f3c7f486c7e27727f75e44519424ff76892e

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,15 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib/hw/android.hardware.camera.provider@2.4-impl.so \
+        |vendor/lib/camera.device@1.0-impl-v27.so \
+        |vendor/lib/camera.device@3.2-impl-v27.so \
+        |vendor/lib/camera.device@3.3-impl-v27.so)
+            "${PATCHELF}" --replace-needed "camera.device@1.0-impl.so" "camera.device@1.0-impl-v27.so" "${2}"
+            "${PATCHELF}" --replace-needed "camera.device@3.2-impl.so" "camera.device@3.2-impl-v27.so" "${2}"
+            "${PATCHELF}" --replace-needed "camera.device@3.3-impl.so" "camera.device@3.3-impl-v27.so" "${2}"
+            "${PATCHELF}" --replace-needed "vendor.qti.hardware.camera.device@1.0_vendor.so" "vendor.qti.hardware.camera.device@1.0.so" "${2}"
+            ;;
         vendor/lib/hw/camera.msm8953.so)
             "${PATCHELF}" --replace-needed "libui.so" "libshims_libui.so" "${2}"
             ;;
