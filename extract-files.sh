e2dce5992fc3cddd4b75aa3b8901251c15585894
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-24 01:49:27 +0100

    vince: Patch camera stack for full VNDK
    Change-Id: I334b51c334ba8bd64e526e62a459d84e671c8230

--- extract-files.sh
+++ extract-files.sh
@@ -18,7 +18,11 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "vendor.qti.hardware.camera.device@1.0_vendor.so" "vendor.qti.hardware.camera.device@1.0.so" "${2}"
             ;;
         vendor/lib/hw/camera.msm8953.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
             "${PATCHELF}" --replace-needed "libui.so" "libshims_libui.so" "${2}"
+            ;;
+        vendor/lib/libFaceGrade.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
             ;;
         vendor/lib/libvidhance_gyro.so)
             "${PATCHELF}" --replace-needed "android.frameworks.sensorservice@1.0.so" "android.frameworks.sensorservice@1.0-v27.so" "${2}"
