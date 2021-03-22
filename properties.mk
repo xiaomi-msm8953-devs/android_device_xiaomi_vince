86f6b36129d105b77de4c101fabe253a1b867830
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-22 10:53:40 +0100

    vince: Disable CDS via property
    Change-Id: I5fc30d41301ac6669c79154ba3d945b66adb7c21

--- properties.mk
+++ properties.mk
@@ -10,3 +10,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
     ro.vendor.audio.sdk.fluencetype=fluence
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.CDS=off \
+    persist.vendor.camera.video.CDS=off
